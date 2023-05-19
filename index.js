50c34d9873ee64790051781dd39c4f205faf7660
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-05-18 22:09:13 -0400

    added env file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// Dependencies
+require('dotenv').config()
 const express = require('express');
 const app = express();
 
+
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(3000)
+app.listen(process.env.PRT)
