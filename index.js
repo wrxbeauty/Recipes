3b4f8f2af4c4f9fff2aec262c4f5caf08b5ccca0
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-06-12 17:44:10 -0400

    Added views engine and installed needed package

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // Dependencies
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
+// Views
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 // Controllers
-app.use('/recipes', require('./controllers/recipes'))
+app.use('/recipes', require('./controllers/recipes'));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.render('home')  // Homepage view
+});
 
 app.get('*', (req, res) => {
     res.status(404).send(`
@@ -17,6 +21,7 @@ app.get('*', (req, res) => {
             <h1 style="text-align: center">404 page</h1>
         </body>
     `)
-})
+});
 
-app.listen(process.env.PRT)
+// Listen
+app.listen(process.env.PRT);
