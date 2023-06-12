127dd7d305da77da0a2699f07b3812c37e874639
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-06-12 17:35:24 -0400

    Added homepage view and function

---
+++ views/home.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const Def = require('./default')
+
+function home() {
+    <Def>
+        <main>
+            <h1>Homepage</h1>
+        </main>
+    </Def>
+}
+
+module.exports = home;
