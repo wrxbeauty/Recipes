a00c14c5d14fc880fad83af7acca357e95eae2bd
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-06-12 12:26:07 -0400

    Updated index.js and notes

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 page</h1>')
+    res.status(404).send(`
+        <body>
+            <h1 style="text-align: center">404 page</h1>
+        </body>
+    `)
 })
 
 app.listen(process.env.PRT)
